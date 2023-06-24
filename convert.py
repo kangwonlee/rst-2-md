4dfd58f7ca78b0002660eb1f99d023ef8fb0cb3b
KangWon LEE <kangwon@gmail.com>
2023-06-24 16:34:13 +0900

    first attempt

---
+++ convert.py
@@ -0,0 +1,68 @@
+"""
+Using commandline utility `pandoc` to convert markdown files to html files.
+
+Usage:
+  python convert.py <path>
+
+"""
+
+import functools
+import pathlib
+import subprocess
+import sys
+
+
+from typing import List, Set
+
+
+def main(argv: List[str]):
+  if len(argv) != 2:
+    print("Usage: python convert.py <path>")
+    sys.exit(1)
+
+  path = pathlib.Path(argv[1])
+  if not path.exists():
+    print("Path does not exist: {}".format(path))
+    sys.exit(1)
+
+  if path.is_dir():
+    convert_dir(path)
+  else:
+    convert_file(path)
+
+
+@functools.lru_cache(maxsize=1)
+def get_ignore_set() -> Set[str]:
+  return set(('.git', '.github'))
+
+
+def is_ignore(path: pathlib.Path) -> bool:
+  return set(path.parts).intersection(get_ignore_set())
+
+
+def convert_dir(path: pathlib.Path):
+  for rst_path in path.glob("**/*.rst"):
+    if not is_ignore(rst_path):
+      convert_file(rst_path)
+
+
+def convert_file(rst_path: pathlib.Path) -> subprocess.CompletedProcess:
+  '''
+  Convert a markdown file to html file.
+
+  pandoc -s -o <output> <input>
+
+  ref : https://stackoverflow.com/questions/45633709/how-to-convert-rst-files-to-md
+  '''
+
+  assert rst_path.is_file(), rst_path
+  assert rst_path.suffix == ".rst", rst_path
+  assert not set(rst_path.parts).intersection(get_ignore_set()), rst_path
+
+  md_path = rst_path.with_suffix(".md")
+  cmd = ("pandoc", "--from", "rst", "-to", "markdown", "-o", str(md_path), str(rst_path))
+  return subprocess.run(cmd)
+
+
+if "__main__" == __name__:
+  main(sys.argv)
